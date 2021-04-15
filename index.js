1c3ed3ed01bad945ae873295bb8d939e48057224
micgreene <mgreene11111@yahoo.com>
2021-04-14 22:21:51 -0700

    changing index.js to test github actions

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get('/', (req,res) => {
 
 app.post('/data', (req,res) => {
   data = req.body;
-  res.status(201).send('Added New Object!');
+  res.status(201).send('Added New Object! Final Remix Volume 10 - Redux');
 });
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
